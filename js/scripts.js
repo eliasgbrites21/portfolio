2e92ca02816f5b1ed465355686ea5ab43153a50d
Elias G. Brites <eliasgbrites@gmail.com>
2022-02-16 09:07:44 -0300

    Transition between dark & light mode added

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ const toggleTheme = document.getElementById('toggle-theme');
 const toggleIcon = document.getElementById('toggle-icon');
 const toggleText = document.getElementById('toggle-text');
 
+const toggleColors=document.getElementById("toggle-colors");
+
+const rootStyles = document.documentElement.style;
+
 toggleTheme.addEventListener('click', ()=>{document.body.classList.toggle('dark');
 if(toggleIcon.src.includes('moon.svg')){
     toggleIcon.src='./assets/icons/sun.svg'
@@ -10,4 +14,8 @@ if(toggleIcon.src.includes('moon.svg')){
     toggleIcon.src='./assets/icons/moon.svg'
     toggleText.textContent='Dark Mode'
 }
-})
+});
+
+toggleColors.addEventListener('click', (e)=>{
+    rootStyles.setProperty("--primary-color", e.target.dataset.color)
+});
